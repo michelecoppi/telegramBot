1ace80b6723846c9012b990679eadf0859214b53
michelecoppi <supergimmy878@gmail.com>
2023-04-11 02:51:55 +0200

    added app

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fs = require('fs');
 let randomWords = require('random-words');
 require('dotenv/config');
 
-const port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 const bot = new TelegramBot(process.env.API_KEY, {polling: true});
 
